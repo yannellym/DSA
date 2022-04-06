c3ab0d3416d45ca68b8372c35a0772aa90be1e04
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-04-06 19:10:55 -0400

    4/6/2022

--- capitalize/index.js
+++ capitalize/index.js
@@ -5,7 +5,9 @@
 //   capitalize("I'm a little tea pot") --> 'I'm A Little Tea Pot'
 //   capitalize('sHoRt AnD sToUt') --> 'Short And Stout'
 
-function capitalize(str) {}
+function capitalize(str) {
+  return str.toLowerCase().split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ") 
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
