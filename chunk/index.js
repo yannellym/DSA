96a86dd321ef8d86c50cf1aff3f083fcb1628eac
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-04-06 18:39:09 -0400

    4/6/2022

--- chunk/index.js
+++ chunk/index.js
@@ -6,8 +6,16 @@
 // chunk(["a", "b", "c", "d"], 2) --> [[ "a", "b"], ["c", "d"]]
 // chunk([0, 1, 2, 3, 4, 5], 4) -->  [[0, 1, 2, 3], [4, 5]]
 
-function chunk(array, size) {}
-
+function chunk(array, size) {
+  let finalArray = [];
+  let index = 0;
+  
+  while(index < array.length){
+    finalArray.push(array.slice(index, index + size))
+    index += size;
+  }
+  return finalArray;
+}
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
 //    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
