b5e24763b505f7b8f4e5e7fd7776b2224944d591
yannelly mercado <yannellym@gmail.com>
2022-05-04 19:18:44 -0400

    reverse a string 5/4

--- reverseString/index.js
+++ reverseString/index.js
@@ -4,7 +4,9 @@
 //   reverse('abcd') === 'dcba'
 //   reverse('Hello!') === '!olleH'
 
-function reverse(str) {}
+function reverse(str) {
+	return str.split("").reverse().join("");
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
